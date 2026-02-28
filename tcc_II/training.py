5c343f9fd534452cfcd59f3f151d71ba93d471a8
gpmatheus <63263215+gpmatheus@users.noreply.github.com>
2026-02-27 22:06:04 -0300

    fix: removed Vmax column selection when

--- tcc_II/training.py
+++ tcc_II/training.py
@@ -39,7 +39,6 @@ def preprocess_image_tf(image, img_w):
 
 def build_dataset(data, batch, img_w):
     imgs, labels = data
-    labels = labels['Vmax']
 
     dataset = tf.data.Dataset.from_tensor_slices((imgs, labels))
     dataset = dataset.repeat()
